db2523d04b9d6b66aa1d213b34a199bca0571236
erkocali1 <muhammedalierkoc92@gmail.com>
2023-06-15 20:40:49 +0300

    LoginViewModel arranged

--- app/src/main/java/com/example/mycomposeapp/feature/login/LoginScreen.kt
+++ app/src/main/java/com/example/mycomposeapp/feature/login/LoginScreen.kt
@@ -9,8 +9,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -27,44 +30,54 @@ internal fun LoginScreenRoute(
     modifier: Modifier = Modifier, viewModel: LoginViewModel = hiltViewModel()
 ) {
     val loginUiState by viewModel.uiState.collectAsStateWithLifecycle()
-    LoginScreen(modifier = modifier, loginUiState = loginUiState)
+    LoginScreen(
+        modifier = modifier,
+        loginUiState = loginUiState,
+        onEmailChangeValue = viewModel::onEmailChange,
+        onPasswordChangeValue = viewModel::onPasswordChange
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(
-    modifier: Modifier = Modifier, loginUiState: LoginUiState
+    modifier: Modifier = Modifier, loginUiState: LoginUiState, onEmailChangeValue: (String) -> Unit,
+    onPasswordChangeValue: (String) -> Unit,
 ) {
     Scaffold {
-        Content(modifier = modifier.padding(it), loginUiState = loginUiState)
+        Content(
+            modifier = modifier.padding(it),
+            loginUiState = loginUiState,
+            onEmailChangeValue = onEmailChangeValue,
+            onPasswordChangeValue = onPasswordChangeValue
+        )
 
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun Content(modifier: Modifier = Modifier, loginUiState: LoginUiState) {
+private fun Content(
+    modifier: Modifier = Modifier, loginUiState: LoginUiState, onEmailChangeValue: (String) -> Unit,
+    onPasswordChangeValue: (String) -> Unit,
+) {
     Column(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize().padding(horizontal = 24.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Login Screen")
         Spacer(modifier = Modifier.height(24.dp))
 
-        OutlinedTextField(
-            label = {
-                Text(text = "Email")
-            },
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Filled.Email,
-                    contentDescription = "Email"
-                )
-            },
+        OutlinedTextField(label = {
+            Text(text = "Email")
+        }, leadingIcon = {
+            Icon(
+                imageVector = Icons.Filled.Email, contentDescription = "Email"
+            )
+        },
 
-            value = loginUiState.email,
-            onValueChange = {}
+            value = loginUiState.email, onValueChange = onEmailChangeValue
         )
         Spacer(modifier = Modifier.height(8.dp))
 
@@ -74,14 +87,24 @@ private fun Content(modifier: Modifier = Modifier, loginUiState: LoginUiState) {
             },
             leadingIcon = {
                 Icon(
-                    imageVector = Icons.Filled.Lock,
-                    contentDescription = "Email"
+                    imageVector = Icons.Filled.Lock, contentDescription = "Password"
                 )
             },
 
-            value = loginUiState.email,
-            onValueChange = {})
+            visualTransformation = PasswordVisualTransformation(),
+            value = loginUiState.password,
+            onValueChange = onPasswordChangeValue
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        OutlinedButton(onClick = { /*TODO*/ }) {
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "Login")
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Divider()
+        OutlinedButton(onClick = { /*TODO*/ }) {
+            Text(text = "Continue With Google")
+        }
     }
-
 
 }
--- app/src/main/java/com/example/mycomposeapp/feature/login/LoginViewModel.kt
+++ app/src/main/java/com/example/mycomposeapp/feature/login/LoginViewModel.kt
@@ -1,18 +1,52 @@
 package com.example.mycomposeapp.feature.login
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mycomposeapp.core.common.Resource
+import com.example.mycomposeapp.core.common.asReSource
+import com.example.mycomposeapp.domain.usecase.login.LoginUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor():ViewModel() {
-    private val _uiState:MutableStateFlow<LoginUiState> = MutableStateFlow(LoginUiState())
-    val uiState=_uiState
+class LoginViewModel @Inject constructor(private val loginUseCase: LoginUseCase) : ViewModel() {
+    private val _uiState: MutableStateFlow<LoginUiState> = MutableStateFlow(LoginUiState())
+    val uiState = _uiState
+
+    fun onEmailChange(email: String) {
+        uiState.value = _uiState.value.copy(email = email)
+    }
+
+    fun onPasswordChange(password: String) {
+        uiState.value = _uiState.value.copy(password = password)
+    }
+
+    fun onLogin() {
+        viewModelScope.launch {
+            loginUseCase(_uiState.value.email, _uiState.value.password).asReSource()
+                .onEach { result ->
+                    when (result) {
+                        is Resource.Loading -> {
+                            _uiState.value = _uiState.value.copy(loading = true)
+                        }
+
+                        is Resource.Error -> {
+                            _uiState.value = _uiState.value.copy(loading = false)
+                        }
+
+                        is Resource.Success -> {
+                            _uiState.value = _uiState.value.copy(loading = false)
+                        }
+
+                    }
+                }
+        }
+    }
 }
 
-    data class LoginUiState(
-        val loading:Boolean=false,
-        val email:String="",
-        val password:String=""
-    )
+data class LoginUiState(
+    val loading: Boolean = false, val email: String = "", val password: String = ""
+)
